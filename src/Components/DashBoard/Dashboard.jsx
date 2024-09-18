c6c776df687d8d6265c8b1a550b655bc00105308
aryan <aryanchidumalla3@gmail.com>
2024-09-19 00:39:37 +0530

    Commit

--- src/Components/DashBoard/Dashboard.jsx
+++ src/Components/DashBoard/Dashboard.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Weather from "./Weather/Weather";
 import NavBar from "../NavBar/NavBar";
 import CropManagement from "./CropManagement/CropManagement";
+import { StyledButton } from "../../App";
+import AddEuipment from "./AddEuipment/AddEquipment";
+import "./Dashboard.css";
 
 export default function Dashboard() {
+  const [addEquipment, setAddEquipment] = useState(false);
+
   return (
     <div>
       <NavBar />
+      {addEquipment && <AddEuipment setAddEquipment={setAddEquipment} />}
       <Weather />
       <CropManagement />
+      <div className="DashboardButtonContainer">
+        <div className="DashboardButtonSubContainer">
+          <div>
+            Ready to earn extra income? Click here to add your equipment for
+            rent!
+          </div>
+          <StyledButton
+            variant="contained"
+            disableElevation
+            disableFocusRipple
+            disableRipple
+            onClick={() => setAddEquipment(true)}
+          >
+            Add Equipment
+          </StyledButton>
+        </div>
+      </div>
     </div>
   );
 }
